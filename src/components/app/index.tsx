26fd010d0feb5015b532e4e06d50aa0328f2c2f1
Clement Lakhal <clementlakhal@gmail.com>
2019-10-03 13:24:27 +0200

    Change the dummy images

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -27,17 +27,13 @@ const App = () => {
   const [images, setImages] = React.useState<string[]>([]);
 
   React.useEffect(() => {
-    async function fetchImages() {
-      const res = await fetch('https://picsum.photos/v2/list');
-      const data = await res.json();
-
-      const imagesBuf: string[] = [];
-
-      for (let i = 0; i < data.length; i += 1) {
-        imagesBuf.push(data[i].download_url);
-      }
-
-      setImages(imagesBuf);
+    function fetchImages() {
+      setImages([
+        'https://preview.redd.it/9inzl7fl03q31.jpg?width=864&auto=webp&s=36a7d23bbc53dfde03f16a93d8f2213cf95bcd08',
+        'https://preview.redd.it/0t6fso9u89q31.jpg?width=576&auto=webp&s=05b386bfa69d8f94bb550e5882b19f03515ac5be',
+        'https://preview.redd.it/n29sz59215q31.jpg?width=576&auto=webp&s=0d1a8d6135f7a70419438263d2ceffa181d8ef19',
+        'https://i.redd.it/ysii047mf8q31.jpg',
+      ]);
     }
 
     fetchImages();
