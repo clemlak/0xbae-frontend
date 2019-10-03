a06615ae08cf16a3ac61d9450f93549678760b63
Clement Lakhal <clementlakhal@gmail.com>
2019-10-03 09:36:40 +0200

    Update Storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,9 @@
 import { configure } from '@storybook/react';
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+const req = require.context('../src', true, /\.stories\.tsx$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
